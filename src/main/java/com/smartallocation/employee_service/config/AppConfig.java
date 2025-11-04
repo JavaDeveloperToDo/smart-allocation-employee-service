a6b1e3dddbdf77960c1ede839e8758b1d06328d3
JavaDeveloperToDo <lokeshfrdamma@gmail.com>
2025-11-04 11:26:15 +0530

    new line added in the config class to test in other

--- src/main/java/com/smartallocation/employee_service/config/AppConfig.java
+++ src/main/java/com/smartallocation/employee_service/config/AppConfig.java
@@ -1,9 +1,15 @@
 package com.smartallocation.employee_service.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-    
+    private static final Logger log = LoggerFactory.getLogger(AppConfig.class);
+
+    {
+        log.info("I am a block from config file");
+    }
 }
